54bdeb7824642392b2c200581fe9794354a1df58
Yuval Mantin <mantin.work@gmail.com>
2023-11-26 00:58:33 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name="backup_syncer",
     version="2.5",
     packages=find_packages(exclude=["tests*"]),
-    description="A files syncer for easy local backups!",
+    description="A local backups creator and syncer, for easy backup handling!",
     long_description=open("README.md").read(),
     install_requires=[
         "shutils",
         "langdetect",
         "tqdm"
     ],
-    url="https://github.com/JesusDMan/JesusDMan",
+    url="https://github.com/JesusDMan/BackupSyncer",
     author="JesusDMan",
     entry_points={"console_scripts": ["bsync = backup_syncer.bin.sync_files:main"]},
 )
